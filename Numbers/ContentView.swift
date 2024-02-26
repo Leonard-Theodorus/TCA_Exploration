9dde8f78a5380f1999804d34a1519b94640ab549
Leonard Theodorus <aoesotfs@gmail.com>
2024-02-26 13:42:27 +0700

    Initial Commit

---
+++ Numbers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Numbers
+//
+//  Created by Alonica🐦‍⬛🐺 on 26/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Numbers/NumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumbersApp.swift
+//  Numbers
+//
+//  Created by Alonica🐦‍⬛🐺 on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
